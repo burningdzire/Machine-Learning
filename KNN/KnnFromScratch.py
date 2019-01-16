68fc5ce86adb6bf5689754d02caa1645403b2687
SharmaMohit <mohit.sharma@tatahealth.com>
2019-01-16 14:48:23 +0530

    DataScience

--- KNN/KnnFromScratch.py
+++ KNN/KnnFromScratch.py
@@ -1,67 +1,63 @@
 # Importing Libraries
 from sklearn.model_selection import train_test_split
-import numpy as np
 import pandas as pd
-from collections import Counter
+import numpy as np
 
-# Importing data
-df = pd.read_csv("Iris.csv")
-df = df.drop(columns=['Id'], axis=1)
+# Importing Data
+df = pd.read_csv('Iris.csv', index_col='Id')
+df.index.name = None
 
-# Partition into features and target
-df_feature = df.iloc[:, df.columns != 'Species'].values
-df_target = df.iloc[:, df.columns == 'Species'].values
-
-
-def Accuracy_Score(pred_target, test_target):
-    if (len(pred_target) != len(test_target)):
-        raise ValueError("pred_target and test_target must be of same length")
-    else:
-        count = 0
-        for i in range(len(test_target)):
-            count += (test_target[i] == pred_target[i])
-        return (count/len(test_target))
+# Partition into attributes and target
+X = df.loc[:, df.columns != 'Species']
+Y = df['Species']
 
 
-def EuclideanDistance(train_feature, test_features):
-    return np.sqrt(np.square(train_feature - test_features))
+# def Accurary_Score(pred_target, test_target):
+#     if (len(pred_target) != len(test_target)):
+#         return -1
+#     else:
+#         count = 0
+#         for i in range(len(pred_target)):
+#             if(pred_target[i] == test_target[i]):
+#                 count += 1
+
+#         return count/len(pred_target)
 
 
 class KNeighborsClassifier:
-
-    def __init__(self, features, target, K=3):
-        self.features = features
-        self.target = target
+    def __init__(self, train_attributes, train_target, K=5):
+        self.train_attributes = train_attributes
+        self.train_target = train_target
         self.K = K
-        self.distance = []
 
-    def featureScaling(self):
-        pass
-    def calculateDistance(self, test_features):
-        test_feature_distance = []
-        for i in range(len(self.features)):
-            dist = np.sqrt(
-                np.sum(np.square(self.features[i, :] - test_features)))
-            test_feature_distance.append([dist, self.target[i, 0]])
+    def calculateTarget(self, test_attribute_index, test_attribute):
+        distance = []
+        for train_attributes_index, train_attribute in self.train_attributes.iterrows():
+            dist = np.sqrt(np.sum(np.square(train_attribute - test_attribute)))
+            distance.append([dist, self.train_target[train_attributes_index]])
 
-        test_feature_distance = sorted(test_feature_distance)
-        labels = []
-        for i in range(self.K):
-            labels.append(test_feature_distance[i][1])
-        return (Counter(labels).most_common(1)[0][0])
+        distance = pd.Series(distance)
+        distance = distance.sort_values(ascending=True)
+        print(distance)
+        # label = []
+        # for i in range(self.K):
+        #     label.append(distance[i][1])
+        # label = pd.Series(label)
+        # return label.value_counts().head(1)
 
-    def predict(self, test_features):
+    def predict(self, test_attributes, test_target):
         pred_target = []
-        for i in range(len(test_features)):
-            pred_target.append(self.calculateDistance(test_features[i, :]))
-        return pred_target
+        for test_attribute_index, test_attribute in test_attributes.iterrows():
+            # pred_target.append(self.calculateTarget(test_attribute_index, test_attribute))
+            self.calculateTarget(test_attribute_index, test_attribute)
+            break
+
+        # print(pred_target)
+        # print(test_target)
+        # return Accurary_Score(pred_target, test_target)
 
 
-# Splitting into training and testing
-train_features, test_features, train_target, test_target = train_test_split(
-    df_feature, df_target, train_size=0.8, test_size=0.2)
-
-KNN = KNeighborsClassifier(train_features, train_target)
-pred_target = KNN.predict(test_features)
-
-print(Accuracy_Score(pred_target, test_target))
+train_attributes, test_attributes, train_target, test_target = train_test_split(
+    X, Y, train_size=0.8, test_size=0.2)
+KNN = KNeighborsClassifier(train_attributes, train_target)
+KNN.predict(test_attributes, test_target)
